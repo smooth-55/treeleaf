e1ca67f52aed0ac2d601fba6a292bc6fa1c33cb2
Ashish <dhakalashish18@gmail.com>
2022-02-05 23:27:47 +0545

    rendering templates

--- treeleaf/views.py
+++ treeleaf/views.py
@@ -1,3 +1,17 @@
+from re import template
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class ProfileListView(TemplateView):
+    template_name = "treeleaf/profile-list.html"
+
+
+class ProfileCreateView(TemplateView):
+    template_name = "treeleaf/profile-create.html"
+
+
+class ProfileUpdateView(TemplateView):
+    template_name = "treeleaf/update-profile.html"
