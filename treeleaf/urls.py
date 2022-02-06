9a395e48eb2a6969efa31ad12949e471db47065c
Ashish <dhakalashish18@gmail.com>
2022-02-06 15:57:15 +0545

    ajax routes

--- treeleaf/urls.py
+++ treeleaf/urls.py
@@ -3,10 +3,16 @@ from treeleaf.views import (
     ProfileCreateView,
     ProfileListView,
     RetrieveProfileView,
+    # CreateSkillView,
+    ajax_skill_create,
+    ajax_skill_update,
 )
 
 urlpatterns = [
     path("create", ProfileCreateView.as_view(), name="create"),
     path("", ProfileListView.as_view(), name="list"),
-    path("view/<int:pk>", RetrieveProfileView.as_view(), name="view"),
+    path("view/<int:pk>/", RetrieveProfileView.as_view(), name="view"),
+    # path("skill/<int:pk>", CreateSkillView.as_view(), name="create-skill"),
+    path("skill/<int:pk>", ajax_skill_create, name="create-skill"),
+    path("skill-update/<int:pk>", ajax_skill_update, name="update-skill"),
 ]
