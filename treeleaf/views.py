7d53875c44603379e742fa0c391b15f373bf4690
Ashish <dhakalashish18@gmail.com>
2022-02-06 13:31:57 +0545

    creating profile with ajax

--- treeleaf/views.py
+++ treeleaf/views.py
@@ -1,8 +1,8 @@
-from re import template
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 from django.views.generic import TemplateView, DetailView
 from django.views.generic.list import ListView
-from treeleaf.models import Profile
+from treeleaf.models import Profile, SkillSet
 
 # Create your views here.
 
@@ -17,5 +17,27 @@ class RetrieveProfileView(DetailView):
     template_name = "treeleaf/view-profile.html"
 
 
-class ProfileCreateView(TemplateView):
-    template_name = "treeleaf/profile-create.html"
+class ProfileCreateView(View):
+    def get(self, request):
+        context = {}
+        return render(request, "treeleaf/profile-create.html", context)
+
+    def post(self, request):
+
+        full_name = request.POST["full_name"]
+        email = request.POST["email"]
+        skill = request.POST["skill"]
+        level = request.POST["level"]
+        photo = request.FILES.get("photo")
+        cv = request.FILES.get("cv")
+
+        create_profile = Profile.objects.create(
+            name=full_name, email=email, cv=cv, photo=photo
+        )
+
+        SkillSet.objects.create(
+            profile=create_profile, skill_name=skill, proficiency_level=level
+        )
+        print("data saved")
+
+        return redirect("/")
