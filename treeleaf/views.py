bac44dddf181369fd64942b3a1684ba83f3f759e
Ashish <dhakalashish18@gmail.com>
2022-02-06 17:08:44 +0545

    profile delete view

--- treeleaf/views.py
+++ treeleaf/views.py
@@ -1,25 +1,36 @@
-from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views import View
-from django.views.generic import TemplateView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, DeleteView
 from django.views.generic.list import ListView
 from treeleaf.models import Profile, SkillSet
-from treeleaf.forms import SkillSetForm
 
 # Create your views here.
 
 
 class ProfileListView(ListView):
+    """
+    Profile list view
+    """
+
     template_name = "treeleaf/profile-list.html"
     model = Profile
 
 
 class RetrieveProfileView(DetailView):
+    """
+    Profile detail view
+    """
+
     model = Profile
     template_name = "treeleaf/view-profile.html"
 
 
 class ProfileCreateView(View):
+    """
+    It Creates the profile and skills with ajax from an asynchronous call
+    """
+
     def get(self, request):
         context = {}
         return render(request, "treeleaf/profile-create.html", context)
@@ -44,13 +55,24 @@ class ProfileCreateView(View):
         return redirect("/")
 
 
+class DeleteProfile(DeleteView):
+    """
+    It will delete user profile
+    """
+
+    model = Profile
+    success_url = reverse_lazy("list")
+
+
 def ajax_skill_create(request, pk):
+    """
+    It will create a skill for a profile with ajax
+    """
     profile = Profile.objects.get(id=pk)
 
     skill_name = request.GET.get("skill")
     prof_level = request.GET.get("level")
 
-    # if request.method == "POST":
     SkillSet.objects.create(
         profile=profile, skill_name=skill_name, proficiency_level=prof_level
     )
@@ -59,12 +81,16 @@ def ajax_skill_create(request, pk):
 
 
 def ajax_skill_update(request, pk):
+    """
+    It will update a skill for a profile with ajax
+    """
 
     skill_name = request.GET.get("skill_name")
     prof_level = request.GET.get("proficiency_level")
 
-    SkillSet.objects.filter(id=pk).update(
-        skill_name=skill_name, proficiency_level=prof_level
-    )
+    skill_object = SkillSet.objects.get(id=pk)
+    skill_object.skill_name = skill_name
+    skill_object.proficiency_level = prof_level
+    skill_object.save()
 
     return redirect("/")
